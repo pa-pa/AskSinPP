38859c51db61b741bdbd3578cd71c594a70a4f98
pa-pa <>
2017-08-10 21:18:23 +0200

    create internal peers for buttons raise peer count to 8

--- examples/HM-LC-SWX-SM/HM-LC-SWX-SM.ino
+++ examples/HM-LC-SWX-SM/HM-LC-SWX-SM.ino
@@ -50,7 +50,7 @@
 #define RELAY4_PIN 3
 
 // number of available peers per channel
-#define PEERS_PER_CHANNEL 4
+#define PEERS_PER_CHANNEL 8
 
 
 // all library classes are placed in the namespace 'as'
@@ -116,6 +116,17 @@ void setup () {
   }
   else {
     DPRINTLN(F("HM-LC-SW4-SM"));
+  }
+  // create internal peerings - CCU2 needs this
+  HMID devid;
+  sdev.getDeviceID(devid);
+  for( uint8_t i=1; i<=sdev.channels(); ++i ) {
+    Peer ipeer(devid,i);
+    // create internal peer if not already done
+    uint8_t idx = 0; // make compiler happy
+    if( sdev.channel(i).peer(idx) != ipeer ) {
+      sdev.channel(i).peer(ipeer);
+    }
   }
 }
 
