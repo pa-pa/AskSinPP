92e1f8c455dd6cc18341359b43846564d164e30f
jp112sdl <jp112sdl@icloud.com>
2018-03-31 11:43:36 +0200

    - added max_retries parameter - return false if measurement fails and max_retries == 0

--- sensors/Dht.h
+++ sensors/Dht.h
@@ -6,35 +6,43 @@
 #include <DHT.h>
 
 namespace as {
-
-// https://github.com/adafruit/DHT-sensor-library
-template <int DATAPIN,int TYPE=DHT11>
-class Dht : public Temperature, public Humidity {
-  DHT _dht;
-
-public:
-  Dht () : _dht(DATAPIN,TYPE) {}
-
-  void init () {
-    _dht.begin();
-    _present = true;
-  }
-  void measure (__attribute__((unused)) bool async=false) {
-    if( present() == true ) {
-        float t = _dht.readTemperature(false);
-        float h = _dht.readHumidity();
-        while (isnan(t) || isnan(h))
-        {
-            DPRINTLN("DHT measure failure. Trying again...");
-            t = _dht.readTemperature(false);
-            h = _dht.readHumidity();
-            delay(500);
+    
+    // https://github.com/adafruit/DHT-sensor-library
+    template <int DATAPIN,int TYPE=DHT11, uint8_t max_retries=0>
+    class Dht : public Temperature, public Humidity {
+        DHT _dht;
+        
+    public:
+        Dht () : _dht(DATAPIN,TYPE) {}
+        
+        void init () {
+            _dht.begin();
+            _present = true;
         }
-        _temperature = t * 10;
-        _humidity = h;
-    }
-  }
-};
+        bool measure (__attribute__((unused)) bool async=false) {
+            if( present() == true ) {
+                uint8_t retries = 0;
+                float t = _dht.readTemperature(false);
+                float h = _dht.readHumidity();
+                
+                if ((isnan(t) || isnan(h)) && max_retries == 0) {
+                    return false;
+                }
+                
+                while ((isnan(t) || isnan(h)) && retries < max_retries)
+                {
+                    retries++;
+                    DPRINT(F("DHT measure failure. Retry #"));DDEC(retries);DPRINT(F(" of "));DDEC(max_retries);DPRINTLN(F(", Trying again..."));
+                    t = _dht.readTemperature(false);
+                    h = _dht.readHumidity();
+                    delay(500);
+                }
+                
+                _temperature = t * 10;
+                _humidity = h;
+            }
+        }
+    };
 }
 
 #endif
