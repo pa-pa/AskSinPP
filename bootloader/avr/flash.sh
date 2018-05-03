71236f90e984b85023965f27c3e686b750b7170c
pa-pa <(none)>
2018-05-03 22:51:30 +0200

    use slower clock for Fuses

--- bootloader/avr/flash.sh
+++ bootloader/avr/flash.sh
@@ -19,6 +19,6 @@ if [ ! -f $BOOTLOADER ]; then
 fi
 
 # Int. RC 8MHz, Brown-out detection disabled
-avrdude -p m328p -P usb -c usbasp -B 3 -U lfuse:w:0xE2:m -U hfuse:w:0xD0:m -U efuse:w:0x06:m -U lock:w:0x2F:m
+avrdude -p m328p -P usb -c usbasp -B 10 -U lfuse:w:0xE2:m -U hfuse:w:0xD0:m -U efuse:w:0x06:m -U lock:w:0x2F:m
 avrdude -p m328p -P usb -c usbasp -V -U flash:w:$BOOTLOADER
 
