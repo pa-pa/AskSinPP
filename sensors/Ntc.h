44a69d03aa0305fcc7b46422f94ffb11ada2fc12
Alexander Reinert <alex@areinert.de>
2018-05-11 17:03:49 +0200

    Added comments for Ntc template parameters
    Use preprocessor conditions for disabling ACTIVATEPIN

--- sensors/Ntc.h
+++ sensors/Ntc.h
@@ -10,6 +10,16 @@
 
 namespace as {
 
+/*
+ * NTC needs to be connected like shown here: http://playground.arduino.cc/uploads/ComponentLib/simple10ktherm_schem_new.png
+
+ * SENSEPIN is the analog pin
+ * ACTIVATEPIN is a output pin to power up the NTC only while measuring. Use 0 if the NTC is connected to VCC and not to a pin.
+ * R0 is the resitance both of the NTC and of the second resistor
+ * B is a material based number of the NTC. Look it up in the datasheet of the NTC. E.g. the ATC 103AT-2 has a B of 3435
+ * T0 is the temperature where the NTC has the resistance R0. In most cases this is 25°C.
+ */
+
 template <int SENSEPIN,int R0=10000,int B=3435,int ACTIVATEPIN=0,int T0=25>
 class Ntc : public Temperature {
   float _b;
@@ -25,16 +35,16 @@ public:
   bool measure (__attribute__((unused)) bool async=false) {
     int vo;
 
-    if (ACTIVATEPIN != 0) {
+#if ACTIVATEPIN != 0
       pinMode(ACTIVATEPIN, OUTPUT);
       digitalWrite(ACTIVATEPIN, HIGH);
-    }
+#endif
 
     vo = analogRead(SENSEPIN);
 
-    if (ACTIVATEPIN != 0) {
+#if ACTIVATEPIN != 0
       digitalWrite(ACTIVATEPIN, LOW);
-    }
+#endif
 
     float rNtc = _r0 * (1023.0 / (float)vo - 1.0);
 
