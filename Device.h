7f1070dd518591e1cabd2db1ef274f8dbeae882d
pa-pa <(none)>
2018-06-07 14:41:45 +0200

    fix wrong size check - bootloader message was not working

--- Device.h
+++ Device.h
@@ -198,7 +198,7 @@ public:
 
   bool pollRadio () {
     uint8_t num = radio().read(msg);
-    if( num > 10 ) { // minimal msg is 10 byte
+    if( num >= 10 ) { // minimal msg is 10 byte
       return process(msg);
     }
     return false;
