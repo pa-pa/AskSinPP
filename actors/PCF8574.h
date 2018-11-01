88f85e5da6252356af41cc37a9e7d8c46537b1de
jp112sdl <jp112sdl@icloud.com>
2018-11-01 20:33:20 +0100

    add PCF8574 port expander as output device

---
+++ actors/PCF8574.h
@@ -0,0 +1,77 @@
+//- -----------------------------------------------------------------------------------------------------------------------
+// AskSin++
+// 2018-11-01 papa Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+// 2018-11-01 jp112sdl Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+//- -----------------------------------------------------------------------------------------------------------------------
+
+#ifndef __PCF8574Output_H__
+#define __PCF8574Output_H__
+
+#include <Arduino.h>
+#include <Wire.h>
+
+namespace as {
+
+struct PCF8574Buffer { uint8_t data, mode; };
+  template <byte ADDRESS=0x38>
+  class PCF8574Output {
+      static PCF8574Buffer& getBuffer();
+  private:
+      static void writeWire(uint8_t buffer) {
+          Wire.beginTransmission(ADDRESS);
+          Wire.write(buffer);
+          Wire.endTransmission();
+      }
+  public:
+      inline static void setOutput   (uint8_t pin) {
+          getBuffer().mode |= bit(pin);
+      }
+      inline static void setInput    (uint8_t pin) {
+          getBuffer().mode &= ~bit(pin);
+      }
+      inline static void setHigh     (uint8_t pin) {
+          getBuffer().data |= bit(pin);
+          writeWire((getBuffer().data & getBuffer().mode));
+      }
+      inline static void setLow      (uint8_t pin) {
+          getBuffer().data &= ~bit(pin);
+          writeWire((getBuffer().data & getBuffer().mode));
+      }
+      inline static uint8_t getState (uint8_t pin) {
+          return ((getBuffer().data & bit(pin)) == 0) ? LOW : HIGH;
+      }
+      inline static void dump () {
+          Serial.print("Address: "); Serial.print(ADDRESS);
+          Serial.print("   Mode: "); Serial.print(getBuffer().mode,BIN);
+          Serial.print("   Data: "); Serial.println(getBuffer().data,BIN);
+      }
+      
+      inline static void init () { 
+      	Wire.begin();
+      }
+  };
+  
+  template<>
+  inline PCF8574Buffer& PCF8574Output<0x38>::getBuffer () {
+      static PCF8574Buffer b;
+      return b;
+  }
+  template<>
+  inline PCF8574Buffer& PCF8574Output<0x40>::getBuffer () {
+      static PCF8574Buffer b;
+      return b;
+  }
+  template<>
+  inline PCF8574Buffer& PCF8574Output<0x42>::getBuffer () {
+      static PCF8574Buffer b;
+      return b;
+  }
+  template<>
+  inline PCF8574Buffer& PCF8574Output<0x44>::getBuffer () {
+      static PCF8574Buffer b;
+      return b;
+  }
+  
+}
+
+#endif
