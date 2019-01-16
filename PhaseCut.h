d58f24664e9b43ad47a6a5470e377131ec9eeb67
SCUBA82 <christian_thalhammer@gmx.at>
2019-01-16 14:52:46 +0100

    Revert "Revert "additional routines for AC dimmers""
    This reverts commit 5d1cf303ce005fffa084c8d8b5f0e74a54da7b33.

---
+++ PhaseCut.h
@@ -0,0 +1,58 @@
+//- -----------------------------------------------------------------------------------------------------------------------
+// AskSin++
+// 2017-03-29 papa Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+// 2019-01-11 scuba82: highly inspired by https://github.com/blackhack/ArduLibraries/tree/master/DimmerOne
+//- -----------------------------------------------------------------------------------------------------------------------
+
+#ifndef _PHASECUT_h
+#define _PHASECUT_h
+
+#ifndef PHASECUTMODE
+#define PHASECUTMODE 0
+#endif 
+
+#ifndef ZEROPIN
+#define ZEROPIN 2
+#endif
+
+#include "Arduino.h"
+//#include "Debug.h"
+
+
+class PhaseCut
+{
+public:
+    PhaseCut();
+
+    void init(uint8_t output_pin);
+
+    bool Start();
+    bool Stop();
+	bool isrunning();
+
+    bool SetDimValue(double value);
+
+    double GetDimValue() { return _timer; }
+
+    // Do not call this!
+    void ZeroCrossEvent();
+    void CmpAEvent();
+	void Fire();
+
+private:
+    void SetTimer();
+
+    uint8_t ZERO_CROSS_PIN;
+    uint8_t OUTPUT_PIN;
+    double _timer;
+
+    bool _valid_zero_crossing;
+    bool isInit;
+	bool running;
+};
+
+void ZeroCrossEventCaller();
+
+extern PhaseCut phaseCut;
+
+#endif
--- actors/PWM.h
+++ actors/PWM.h
@@ -6,7 +6,12 @@
 #ifndef __PWM_H__
 #define __PWM_H__
 
+
 #include <Arduino.h>
+#include "PhaseCut.h"
+
+
+
 
 namespace as {
 
@@ -14,7 +19,12 @@ namespace as {
 // we use this table for the dimmer levels
 static const uint8_t pwmtable[32] PROGMEM = {
     1, 1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 8, 10, 11, 13, 16, 19, 23,
-    27, 32, 38, 45, 54, 64, 76, 91, 108, 128, 152, 181, 215, 255
+    27, 32, 38, 45, 54, 64, 76, 91, 108, 128, 152, 181, 200, 255
+};
+static const uint8_t zctable[45] PROGMEM = {
+     11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
+	 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+	 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 58, 75 
 };
 template<uint8_t STEPS=200>
 class PWM8 {
@@ -35,6 +45,37 @@ public:
     }
     analogWrite(pin,pwm);
   }
+};
+template<uint8_t STEPS=200>
+class ZC_Control {
+	uint8_t  outpin;
+	public:
+	ZC_Control () : outpin(0) {}
+	~ZC_Control () {}
+		
+	void init(uint8_t p) {
+    outpin = p;
+    phaseCut.init(outpin);
+	phaseCut.Start();
+  }
+  void set(double value){
+		uint8_t pwm = 0;
+		if ( value > 0 ) {
+			if (!phaseCut.isrunning()){
+				phaseCut.Start();
+			}
+			#if PHASECUTMODE == 1
+				uint8_t offset = (STEPS - value)*44/STEPS;
+			#else
+				uint8_t offset = value*44/STEPS;
+			#endif
+			pwm = pgm_read_word (& zctable[offset]);
+			phaseCut.SetDimValue(pwm);
+		}
+	  else{
+		phaseCut.Stop();
+	  }  
+   }
 };
 #endif
 
@@ -63,10 +104,7 @@ public:
     }
     else if (value > 0) {
       // https://diarmuid.ie/blog/pwm-exponential-led-fading-on-arduino-or-other-platforms/
-      // duty = pow(2,(value/R)) + 4;
-      // duty = pow(2,(value/20.9)+6.5);
-      // duty = pow(1.37,(value/15.0)+22.0)-500;
-      duty = pow(1.28,(value/13.0)+29.65)-1300;
+      duty = pow(2,(value/R)) + 4;
       // http://harald.studiokubota.com/wordpress/index.php/2010/09/05/linear-led-fading/index.html
       //duty = exp(value/18.0) + 4;
     }
