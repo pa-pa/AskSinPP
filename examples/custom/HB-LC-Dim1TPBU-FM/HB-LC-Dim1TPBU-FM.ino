f39ff42cfac053d2ee59d06f3de8ab6ed3d1c594
SCUBA82 <christian_thalhammer@gmx.at>
2019-01-16 14:31:02 +0100

    example and FHEM support for HB-LC-Dim1TPBU-FM

---
+++ examples/custom/HB-LC-Dim1TPBU-FM/HB-LC-Dim1TPBU-FM.ino
@@ -0,0 +1,172 @@
+//- -----------------------------------------------------------------------------------------------------------------------
+// AskSin++
+// 2016-10-31 papa Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+// 2018-10-07 jp112sdl Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+// 2019-01-10 scuba82 Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+//- -----------------------------------------------------------------------------------------------------------------------
+
+// define this to read the device id, serial and device type from bootloader section
+//#define USE_OTA_BOOTLOADER
+
+// Arduino IDE Settings
+// use support for 644PA from MightyCore: https://github.com/MCUdude/MightyCore
+// settings:
+// Board:        ATMega644
+// Pinout:       Standard
+// Clock:        8MHz external
+// Variant:      644
+// BOD:          2.7V
+// Compiler LTO: Enabled
+
+#define NDEBUG
+
+// use external EEProm 24c32 via I2C 
+#define STORAGEDRIVER at24cX<0x50,128,32> 
+
+#define EI_NOTEXTERNAL
+#include <Wire.h>
+#include <EnableInterrupt.h>
+#include <AskSinPP.h>
+#include <Dimmer.h>
+#include <Remote.h>
+#include <Sensors/Ntc.h>
+
+// PIN definitions
+#define DIMMERPIN          22  //PC6
+#define BTN_PIN_1          23  //8  //PD0(RxD)
+#define BTN_PIN_2          12  //PD4 
+#define LED_PIN             0  //PB0
+#define CONFIG_BUTTON_PIN  15  //PD7
+#define GDO0_PIN           10  //PD2
+#define LOADPIN            27  //PA4
+#define ZEROPIN             2  //PB2
+#define NTC_SENSE_PIN      24  //PA0  pin to measure ntc
+#define NTC_ACTIVATOR_PIN   0  // pin to power ntc (or 0 if connected to vcc)
+
+// NTC definitions
+#define NTC_T0 25 // temperature where ntc has resistor value of R0
+#define NTC_R0 10000 // resistance both of ntc and known resistor
+#define NTC_B 3435 // b value of ntc (see datasheet)
+#define NTC_OVERSAMPLING 2 // number of additional bits by oversampling (should be between 0 and 6, highly increases number of measurements)
+
+// Phase Cut mode 
+#define PHASECUT_MODE 0 // 0 = trailing-edge phase cut; 1 = leading-edge phase cut
+
+// number of available peers per channel
+#define PEERS_PER_DimChannel     4
+#define PEERS_PER_RemoteChannel  8
+
+// all library classes are placed in the namespace 'as'
+using namespace as;
+
+// define all device properties
+const struct DeviceInfo PROGMEM devinfo = {
+    {0x11,0x12,0x99},       // Device ID
+    "scub111299",           // Device Serial
+    {0xF2,0x99},            // Device Model
+    0x25,                   // Firmware Version
+    as::DeviceType::Dimmer, // Device Type
+    {0x01,0x00}             // Info Bytes
+};
+/**
+   Configure the used hardware
+*/
+typedef AvrSPI<4,5,6,7> SPIType;
+typedef Radio<SPIType, GDO0_PIN> RadioType;
+typedef StatusLed<LED_PIN> LedType;
+typedef AskSin<LedType, NoBattery, RadioType> Hal;
+Hal hal;
+
+DEFREGISTER(Reg0, MASTERID_REGS, DREG_INTKEY, DREG_CYCLICINFOMSG)
+class DmList0 : public RegList0<Reg0> {
+  public:
+    DmList0(uint16_t addr) : RegList0<Reg0>(addr) {}
+    void defaults() {
+      clear();
+      intKeyVisible(true);
+    }
+};
+
+typedef DimmerChannel<Hal, PEERS_PER_DimChannel, DmList0> DimChannel;
+typedef RemoteChannel<Hal, PEERS_PER_RemoteChannel, DmList0> RmtChannel;
+typedef DimmerAndRemoteDevice<Hal, DimChannel, RmtChannel, 3, 3, 2, DmList0> DimDevice;
+
+
+
+DimDevice sdev(devinfo, 0x20);
+DimmerControl<Hal,DimDevice,ZC_Control<>> control(sdev);
+ConfigButton<DimDevice> cfgBtn(sdev);
+
+class OverloadSens : public Alarm {
+  uint8_t overloadcount = 0;
+  uint8_t counter;
+  public:
+  OverloadSens () : Alarm(0) {}
+  virtual ~OverloadSens () {}
+  void init () {
+    pinMode(LOADPIN,INPUT);
+    set(seconds2ticks(15));
+    sysclock.add(*this);
+  }
+  virtual void trigger (AlarmClock& clock) {
+     bool senspin = digitalRead(LOADPIN);
+     control.setOverload(senspin);
+     set(seconds2ticks(1));
+     clock.add(*this);
+  }
+};
+OverloadSens overload;
+
+class TempSens : public Alarm {
+  Ntc<NTC_SENSE_PIN,NTC_R0,NTC_B,NTC_ACTIVATOR_PIN,NTC_T0,NTC_OVERSAMPLING> ntc;
+  
+  public:
+  TempSens () : Alarm(0) {}
+  virtual ~TempSens () {}
+
+  void init () {
+    ntc.init();
+    set(seconds2ticks(15));
+    sysclock.add(*this);
+  }
+
+  virtual void trigger (AlarmClock& clock) {
+    ntc.measure();
+    DPRINT("Temp: ");DDECLN(ntc.temperature());
+    control.setTemperature(ntc.temperature());
+    set(seconds2ticks(2));
+    clock.add(*this);
+  }
+};
+TempSens tempsensor;
+
+
+void initPeerings (bool first) {
+  if ( first == true ) {
+    HMID devid;
+    sdev.getDeviceID(devid);
+    sdev.dimmerChannel(1).peer(Peer(devid, 1), Peer(devid, 2));
+    sdev.remoteChannel(1).peer(Peer(devid, 3));
+  	sdev.remoteChannel(2).peer(Peer(devid, 3));
+  }
+}
+
+
+void setup () {
+  DINIT(57600, ASKSIN_PLUS_PLUS_IDENTIFIER);
+  bool first = control.init(hal,DIMMERPIN);
+  
+  remoteChannelISR(sdev.remoteChannel(1), BTN_PIN_1);
+  remoteChannelISR(sdev.remoteChannel(2), BTN_PIN_2);
+  buttonISR(cfgBtn, CONFIG_BUTTON_PIN);
+  tempsensor.init();
+  overload.init();
+  initPeerings(first);
+
+  sdev.initDone();
+}
+
+void loop() {
+  bool worked = hal.runready();
+  bool poll = sdev.pollRadio();
+}
--- examples/custom/contrib/FHEM/HMConfig_AskSinPPCustom.pm
+++ examples/custom/contrib/FHEM/HMConfig_AskSinPPCustom.pm
@@ -35,6 +35,15 @@ $HMConfig::culHmRegChan {"HM-LC-Sw2-FM-CustomFW02"} = $HMConfig::culHmRegType{re
 $HMConfig::culHmRegChan {"HM-LC-Sw2-FM-CustomFW03"} = $HMConfig::culHmRegType{switch};
 $HMConfig::culHmRegChan {"HM-LC-Sw2-FM-CustomFW04"} = $HMConfig::culHmRegType{switch};
 
+$HMConfig::culHmModel{"F335"} = {name=>"HM-LC-Sw1-FM-Cus",st=>'custom',cyc=>'',rxt=>'',lst=>'1,3:3p,3:1p.2p',chn=>"Btn:1:2,Sw:3:3"};
+$HMConfig::culHmChanSets{"HM-LC-Sw1-FM-Cus00"}{fwUpdate} = "<filename>";
+$HMConfig::culHmChanSets{"HM-LC-Sw1-FM-Cus01"} = $HMConfig::culHmSubTypeSets{"THSensor"};
+$HMConfig::culHmChanSets{"HM-LC-Sw1-FM-Cus02"} = $HMConfig::culHmSubTypeSets{"THSensor"};
+$HMConfig::culHmChanSets{"HM-LC-Sw1-FM-Cus03"} = $HMConfig::culHmSubTypeSets{"switch"};
+$HMConfig::culHmRegChan {"HM-LC-Sw1-FM-Cus01"} = $HMConfig::culHmRegType{remote};
+$HMConfig::culHmRegChan {"HM-LC-Sw1-FM-Cus02"} = $HMConfig::culHmRegType{remote};
+$HMConfig::culHmRegChan {"HM-LC-Sw1-FM-Cus03"} = $HMConfig::culHmRegType{switch};
+
 $HMConfig::culHmModel{"F202"} = {name=>"HB-SW2-SENS",st=>'custom',cyc=>'',rxt=>'',lst=>'1,3:1p.2p,4:3p',chn=>"Sw:1:2,Sen:3:3"};
 $HMConfig::culHmChanSets{"HB-SW2-SENS00"}{fwUpdate} = "<filename>";
 $HMConfig::culHmChanSets{"HB-SW2-SENS01"} = $HMConfig::culHmSubTypeSets{"switch"};
@@ -101,6 +110,28 @@ $HMConfig::culHmChanSets{"HB-UNI-Sen-LEV-US01"} = {};
 $HMConfig::culHmRegChan {"HB-UNI-Sen-LEV-US"}   = { pairCentral=>1, lowBatLimit=>1, sendInterval=>1 };
 $HMConfig::culHmRegChan {"HB-UNI-Sen-LEV-US01"} = { distanceOffset=>1, sensorType=>1, caseDesign=>1, caseHeight=>1, caseWidth=>1, caseLength=>1 };
 
+#Custom registers for HB-LC-Dim1PBU-FM
+for my $reading0 (keys %{$HMConfig::culHmRegType{dimmer}}) {
+
+	$HMConfig::culHmRegType{custom_dimmer}{$reading0} = 1;
+}
+for my $reading1 (keys %{$HMConfig::culHmRegModel{"HM-LC-Dim1TPBU-FM"}}) {
+
+		$HMConfig::culHmRegType{custom_dimmer}{$reading1} = 1;
+}
+
+$HMConfig::culHmModel{"F299"} = {name=>"HB-LC-Dim1PBU-FM",st=>'custom',cyc=>'',rxt=>'',lst=>'1,3:3p.4p.5p,4:1p.2p',chn=>"Btn:1:2,Dim:3:3,Dim_V:4:5"};
+$HMConfig::culHmChanSets{"HB-LC-Dim1PBU-FM00"}{fwUpdate} = "<filename>";
+$HMConfig::culHmChanSets{"HB-LC-Dim1PBU-FM01"} = $HMConfig::culHmSubTypeSets{"THSensor"};
+$HMConfig::culHmChanSets{"HB-LC-Dim1PBU-FM02"} = $HMConfig::culHmSubTypeSets{"THSensor"};
+$HMConfig::culHmChanSets{"HB-LC-Dim1PBU-FM03"} = $HMConfig::culHmSubTypeSets{"dimmer"};
+$HMConfig::culHmChanSets{"HB-LC-Dim1PBU-FM04"} = $HMConfig::culHmSubTypeSets{"dimmer"};
+$HMConfig::culHmChanSets{"HB-LC-Dim1PBU-FM05"} = $HMConfig::culHmSubTypeSets{"dimmer"};
+$HMConfig::culHmRegChan {"HB-LC-Dim1PBU-FM01"} = $HMConfig::culHmRegType{remote};
+$HMConfig::culHmRegChan {"HB-LC-Dim1PBU-FM02"} = $HMConfig::culHmRegType{remote};
+$HMConfig::culHmRegChan {"HB-LC-Dim1PBU-FM03"} = $HMConfig::culHmRegType{custom_dimmer};
+$HMConfig::culHmRegChan {"HB-LC-Dim1PBU-FM04"} = $HMConfig::culHmRegType{custom_dimmer};
+$HMConfig::culHmRegChan {"HB-LC-Dim1PBU-FM05"} = $HMConfig::culHmRegType{custom_dimmer};
 
 sub HMConfig_AskSinPPCustom_Initialize () {
   # Log 1,"Rerun Literal reverse mapping";
@@ -146,8 +177,8 @@ sub CUL_HM_Parsecustom($$$$$$) {
   # handle Ack_Status or Info_Status message here
   if (($mTp eq "02" && $p =~ m/^01/) || ($mTp eq "10" && $p =~ m/^06/) ) {
     # split payload
-    my ($subType,$chn,$val,$err) = ($1,hex($2),hex($3)/2,hex($4))
-                        if($p =~ m/^(..)(..)(..)(..)/);
+    my ($subType,$chn,$val,$err,$xx,$pl) = ($1,hex($2),hex($3)/2,hex($4),hex($5),hex($6)/2)
+                        if($p =~ m/^(..)(..)(..)(..)(..)(..)/);
     $chn = sprintf("%02X",$chn&0x3f);
   	my $chId = $src.$chn;
     my $shash = $modules{CUL_HM}{defptr}{$chId}
@@ -172,7 +203,119 @@ sub CUL_HM_Parsecustom($$$$$$) {
       push @evtEt,[$chnHash,1,"state:".$vs];
 	    push @evtEt,[$chnHash,1,"contact:$vs$target"];
 	  }
-  }
+
+  # update state of a dimmer
+
+     if( $HMConfig::culHmRegChan{$model.$chn} == $HMConfig::culHmRegType{dimmer} ||  $HMConfig::culHmRegChan{$model.$chn} == $HMConfig::culHmRegType{custom_dimmer}) {
+	 	 
+	my $mId = CUL_HM_getMId($shash);
+	my $chnRemMax;
+	my $rSUpdt = 0;# require status update
+	my $cName = CUL_HM_id2Name($src.sprintf("%02X",$chn));
+    my($lvlMin,$lvlMax)=split",",AttrVal($cName, "levelRange", "0,100");
+	my $physLvl; 
+	my $vDim = $shash->{helper}{vDim}; #shortcut
+	
+	#do we have remote channels ?
+	if ($HMConfig::culHmModel{$mId} && $HMConfig::culHmModel{$mId}{chn} =~ m/Btn/){
+		my @chnRem = (grep{$_ =~ m/Btn:/ } split ',',$HMConfig::culHmModel{$mId}{chn});
+		@chnRem = split ':',$chnRem[0] if (@chnRem);
+		$chnRemMax = $chnRem[2];
+	}
+	else{
+		$chnRemMax = 0;
+	}
+	if ($HMConfig::culHmModel{$mId} && $HMConfig::culHmModel{$mId}{chn} =~ m/Dim_V/){
+		my @chnPh = (grep{$_ =~ m/Dim:/ } split ',',$HMConfig::culHmModel{$mId}{chn});
+        @chnPh = split ':',$chnPh[0] if (@chnPh);
+        my $chnPhyMax = $chnPh[2]?$chnPh[2] - $chnRemMax:1;         # max Phys channels
+        my $chnPhy    = ($chnPhyMax == 2 && $chn >  4 + $chnRemMax)?2:1;    # assotiated phy chan( either 1 or 2)
+        my $idPhy     = $src.sprintf("%02X",$chnPhy + $chnRemMax);# ID assot phy chan
+        my $pHash     = CUL_HM_id2Hash($idPhy);        # hash assot phy chan
+        $idPhy        = $pHash->{DEF};                 # could be device!!!
+        if ($pHash){
+          $pHash->{helper}{vDim}{idPhy} = $idPhy;
+          my $vHash = CUL_HM_id2Hash($src.sprintf("%02X",$chnPhyMax+(2 + $chnRemMax)*$chnPhy-1));
+          if ($vHash){
+            $pHash->{helper}{vDim}{idV2}  = $vHash->{DEF};
+            $vHash->{helper}{vDim}{idPhy} = $idPhy;
+          }
+          else{
+            delete $pHash->{helper}{vDim}{idV2};
+          }
+          $vHash = CUL_HM_id2Hash($src.sprintf("%02X",$chnPhyMax+(2 + $chnRemMax)*$chnPhy));
+          if ($vHash){
+            $pHash->{helper}{vDim}{idV3}  = $vHash->{DEF};
+            $vHash->{helper}{vDim}{idPhy} = $idPhy;
+          }
+          else{
+            delete $pHash->{helper}{vDim}{idV3};
+          }
+        }
+    }	
+	if ($vDim->{idPhy} &&
+            CUL_HM_id2Hash($vDim->{idPhy})){   #has virt chan
+          RemoveInternalTimer("sUpdt:".$chId);
+          if ($mTp eq "10"){               #valid PhysLevel
+            foreach my $tmpKey ("idPhy","idV2","idV3",){#update all virtuals
+              my $vh = ($vDim->{$tmpKey} ? CUL_HM_id2Hash($vDim->{$tmpKey}) : "");
+              next if (!$vh || $vDim->{$tmpKey} eq $chId);
+              my $vl = ReadingsVal($vh->{NAME},"level","???");
+              my $vs = ($vl eq "100"?"on":($vl eq "0"?"off":"$vl"));
+              my($clvlMin,$clvlMax)=split",",AttrVal($vh->{NAME}, "levelRange", "0,100");
+              my $plc = int((($pl-$clvlMin)*200)/($clvlMax - $clvlMin))/2;
+              $plc = 1 if ($pl && $plc <= 0);
+              $vs = ($plc ne $vl)?"chn:$vs  phys:$plc":$vs;
+              push @evtEt,[$vh,1,"state:$vs"];
+              push @evtEt,[$vh,1,"phyLevel:$pl"];
+            }
+            push @evtEt,[$shash,1,"phyLevel:$pl"];      #phys level,don't use relative adjustment
+            $pl = (($pl-$lvlMin)<=0 && $pl)
+                     ? ($pl?1:0)
+                     : int((($pl-$lvlMin)*200)/($lvlMax - $lvlMin))/2;
+            $physLvl = $pl;
+          }
+          else{                                #invalid PhysLevel
+            $rSUpdt = 1;
+            CUL_HM_stateUpdatDly($cName,5) if ($shash->{helper}{dlvl});# update to get level
+          }
+        }
+	my $pVal = $val;# necessary for oper 'off', not logical off
+       $val = (($val-$lvlMin)<=0)
+                  ? ($val?1:0)
+                  : int((($val-$lvlMin)*200)/($lvlMax - $lvlMin))/2;
+	
+	if(!defined $physLvl){             #not updated? use old or ignore
+        $physLvl = ReadingsVal($cName,"phyLevel",$val);
+        $physLvl = (($physLvl-$lvlMin)<=0 && $physLvl)
+                 ? ($physLvl?1:0)
+                 : int((($physLvl-$lvlMin)*200)/($lvlMax - $lvlMin))/2;
+      }
+	 
+	 my $vs = ($shash->{helper}{lm} && $shash->{helper}{lm}{$val})
+                     ?$shash->{helper}{lm}{$val}
+                     :($val==100 ? "on"
+                                 :($pVal==0 ? "off"
+                                            : "$val")); # user string...
+	 
+											
+	 my ($timedOn,$stateExt)=("off","");
+      if($err&0x40){
+        $timedOn = "running";
+        $stateExt = "-till" if(AttrVal($cName,"param","") =~ m/showTimed/ );
+      }
+      push @evtEt,[$shash,1,"level:$val"];
+      push @evtEt,[$shash,1,"pct:$val"]; # duplicate to level - necessary for "slider"
+      push @evtEt,[$shash,1,"deviceMsg:$vs$target"] if($chId ne "00");
+      push @evtEt,[$shash,1,"state:".(($physLvl ne $val)?"chn:$vs phys:$physLvl":$vs.$stateExt)];      
+	  push @evtEt,[$shash,1,"overload:".(($err&0x02)?"on":"off")];
+      push @evtEt,[$shash,1,"overheat:".(($err&0x04)?"on":"off")];
+      push @evtEt,[$shash,1,"reduced:" .(($err&0x08)?"on":"off")];
+	  push @evtEt,[$shash,1,"timedOn:".(($err&0x40)?"running":"off")];
+	  
+    }
+
+}
   # handle sensor event
   elsif($mTp =~ m/^41/ && $p =~ m/^(..)(..)(..)/) {
     my ($chn,$cnt,$val) = (hex($1),hex($2),hex($3)/2);
