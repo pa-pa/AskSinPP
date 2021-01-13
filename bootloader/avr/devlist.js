7cf08beb3e11e927f067ac5ea5b76f017cf694e5
pa-pa <(none)>
2021-01-13 22:06:32 +0100

    name fixed

--- bootloader/avr/devlist.js
+++ bootloader/avr/devlist.js
@@ -5,7 +5,7 @@ var devlist = {
     { "id" : "000A", "name" : "HM-LC-SW2-SM" },
     { "id" : "0003", "name" : "HM-LC-SW4-SM" },
     { "id" : "0005", "name" : "HM-LC-Bl1-FM" },
-    { "id" : "001D", "name" : "HM-RC-3" },
+    { "id" : "001D", "name" : "HM-RC-Key3" },
     { "id" : "0030", "name" : "HM-SEC-RHS" },
     { "id" : "00C3", "name" : "HM-SEC-RHS-2" }
   ],
