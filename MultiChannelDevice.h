6d3935e8f8685c8f062839874f5ba934a3a6ee3f
jp112sdl <jp112sdl@icloud.com>
2022-07-13 07:48:30 +0200

    disable led when an info message is sent

--- MultiChannelDevice.h
+++ MultiChannelDevice.h
@@ -190,7 +190,10 @@ public:
     for( uint8_t i=1; i<=this->channels(); ++i ) {
       ChannelType& ch = channel(i);
       if( ch.changed() == true ) {
+        bool ledEnableState = hal.led.enable(); //save current enable state
+        hal.led.enable(false); //deactivate LED when sending info message (cyclic, sabotage, ...)
         this->sendInfoActuatorStatus(this->getMasterID(),this->nextcount(),ch);
+        hal.led.enable(ledEnableState); //restore enable state
         worked = true;
       }
     }
