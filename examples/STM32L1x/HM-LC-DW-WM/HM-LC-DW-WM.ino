097b600ef20a86a11f926e85a5fb0d87a7a4f39f
trilu2000 <trilu@gmx.de>
2023-10-12 12:49:49 +0200

    cleanup dimmer sketch

--- examples/STM32L1x/HM-LC-DW-WM/HM-LC-DW-WM.ino
+++ examples/STM32L1x/HM-LC-DW-WM/HM-LC-DW-WM.ino
@@ -20,22 +20,20 @@
 HardwareSerial Serial1(PA10, PA9);
 #define DSERIAL Serial1
 
-#define PIN_SPI_MOSI       PB_15
-#define PIN_SPI_MISO       PB_14
-#define PIN_SPI_SCK        PB_13
-#define CC1101_GDO0_PIN    PA_8
-#define CC1101_CS_PIN      PB_12
-#define CC1101_EN_PIN      PA_15
+#define PIN_SPI_MOSI       PB15
+#define PIN_SPI_MISO       PB14
+#define PIN_SPI_SCK        PB13
+#define CC1101_GDO0_PIN    PA8
+#define CC1101_CS_PIN      PB12 // arduino pin 20
+#define CC1101_EN_PIN      PA15
 
 // needed for cpu temp measurement function
 #include "stm32yyxx_ll_adc.h"
 // ------------------------------------------------
 
-
 #include <SPI.h>
 #include <AskSinPP.h>
 #include <Dimmer.h>
-
 
 // Pin definition of the specific device
 #define CONFIG_BUTTON_PIN   PC15
@@ -43,9 +41,6 @@ HardwareSerial Serial1(PA10, PA9);
 #define LED2_PIN            PC13
 #define DIMMER1_PIN         PB4
 #define DIMMER2_PIN         PB5
-
-//#define BTN_PIN_1           PB6
-//#define BTN_PIN_2           PB7
 
 // number of available peers per channel
 #define PEERS_PER_CHANNEL 10
@@ -59,7 +54,6 @@ const struct DeviceInfo PROGMEM devinfo = {
   SERIAL_ID,              // Device Serial
   
   {0x01,0x08},            // Device Model: HM-LC-DW-WM dual white LED dimmer
-  //{0x00,0x67},          // Device Model: HM-LC-Dim1PWM-CV
   0x2C,                   // Firmware Version
   as::DeviceType::Dimmer, // Device Type
   {0x01,0x00}             // Info Bytes
@@ -67,27 +61,20 @@ const struct DeviceInfo PROGMEM devinfo = {
 
 // Configure the used hardware
 typedef LibSPI<CC1101_CS_PIN> RadioSPI;
-//typedef Radio<RadioSPI, CC1101_GDO0_PIN> RadioType;
 typedef CalibratedRadio<RadioSPI, CC1101_GDO0_PIN, CC1101_EN_PIN> RadioType;
 typedef DualStatusLed<LED1_PIN, LED2_PIN> LedType;
 typedef AskSin<LedType, NoBattery, RadioType> HalType;
 typedef DimmerChannel<HalType,PEERS_PER_CHANNEL> ChannelType;
-typedef DimmerDevice<HalType, ChannelType, 6, 3> DimmerType;     // HM-LC-DW-WM dual white LED dimmer
-//typedef DimmerDevice<HalType, ChannelType, 3, 3> DimmerType;   // HM-LC-Dim1PWM-CV
+typedef DimmerDevice<HalType, ChannelType, 6, 3> DimmerType;    
 
 HalType hal;
 DimmerType sdev(devinfo,0x20);
-DualWhiteControl<HalType, DimmerType, PWM16<> > control(sdev);   // HM-LC-DW-WM dual white LED dimmer
-//DimmerControl<HalType,DimmerType, PWM16<> > control(sdev);     // HM-LC-Dim1PWM-CV
+DualWhiteControl<HalType, DimmerType, PWM16<> > control(sdev);   
 
 ConfigToggleButton<DimmerType> cfgBtn(sdev);
-//ConfigButton<DimmerType> cfgBtn(sdev);
-//InternalButton<DimmerType, LOW, HIGH, INPUT_PULLDOWN> btn1(sdev, 1);
-//InternalButton<DimmerType, LOW, HIGH, INPUT_PULLDOWN> btn2(sdev, 2);
 
 
-// internal cpu reading function for STM32L1x
-
+// depends on internal adc cpu reading function of STM32L1x
 class TempSens : public Alarm {
   uint16_t vref;
   uint16_t temp;
@@ -130,17 +117,13 @@ TempSens tempsensor;
 
 
 void setup () {
-
   //storage().setByte(0, 0);
   delay(1000);
   DINIT(57600,ASKSIN_PLUS_PLUS_IDENTIFIER);
 
-  bool first = control.init(hal, DIMMER1_PIN, DIMMER2_PIN);      // HM-LC-DW-WM dual white LED dimmer
-  //bool first = control.init(hal, DIMMER1_PIN);                 // HM-LC-Dim1PWM-CV
+  bool first = control.init(hal, DIMMER1_PIN, DIMMER2_PIN);      
   buttonISR(cfgBtn, CONFIG_BUTTON_PIN);
-  //buttonISR(btn1, BTN_PIN_1);
-  //buttonISR(btn2, BTN_PIN_2);
-
+ 
   if (first == true) {
     sdev.channel(1).peer(cfgBtn.peer());
     sdev.channel(2).peer(cfgBtn.peer());
@@ -148,13 +131,6 @@ void setup () {
     sdev.channel(4).peer(cfgBtn.peer());
     sdev.channel(5).peer(cfgBtn.peer());
     sdev.channel(6).peer(cfgBtn.peer());
-
-    //sdev.channel(1).peer(btn1.peer());
-    //sdev.channel(2).peer(btn2.peer());
-    //sdev.channel(3).peer(btn1.peer());
-    //sdev.channel(4).peer(btn2.peer());
-    //sdev.channel(5).peer(btn1.peer());
-    //sdev.channel(6).peer(btn2.peer());
   }
 
   tempsensor.init();
@@ -167,7 +143,8 @@ void loop() {
   bool worked = hal.runready();
   bool poll = sdev.pollRadio();
   if( worked == false && poll == false ) {
-    hal.activity.savePower<Idle<true> >(hal);
+    // needs power off cycle after upload
+    hal.activity.savePower<Idle<true> >(hal); 
   }
 
 }
